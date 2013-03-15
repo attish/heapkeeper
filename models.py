893c6fdec38a3d35e01b2db50204868848c9b4a1
Attila István NAGY <me@nagyattilaistvan.hu>
2013-03-15 23:48:51 +0100

    Bug in memoize reset method fixed
    [bugfix]
    
    This bug essentially made the reset operation do nothing, which caused
    stale mappings to appear in the memoize database, which caused weird
    type errors that spontaneously disappeared after a server restart.

--- models.py
+++ models.py
@@ -38,34 +38,40 @@ def get_or_make_label_obj(label):
         
 # Credits to http://stackoverflow.com/questions/4431703
 class memoized(object):
-   """Decorator that caches a function's return value each time it is
-   called.  If called later with the same arguments, the cached value
-   is returned, and not re-evaluated.
-   """
-   def __init__(self, func):
-      self.func = func
-      self.cache = {}
-   def __call__(self, *args):
-      try:
-         return self.cache[args]
-      except KeyError:
-         value = self.func(*args)
-         self.cache[args] = value
-         return value
-      except TypeError:
-         # uncachable -- for instance, passing a list as an argument.
-         # Better to not cache than to blow up entirely.
-         return self.func(*args)
-   def __repr__(self):
-      """Return the function's docstring."""
-      return self.func.__doc__
-   def __get__(self, obj, objtype):
-      """Support instance methods."""
-      fn = functools.partial(self.__call__, obj)
-      fn.reset = self._reset
-      return fn
-   def _reset(self):
-      self.cache.pop(self, None)
+    """Decorator that caches a function's return value each time it is
+    called.  If called later with the same arguments, the cached value
+    is returned, and not re-evaluated.
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+            # uncachable -- for instance, passing a list as an argument.
+            # Better to not cache than to blow up entirely.
+            return self.func(*args)
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        fn = functools.partial(self.__call__, obj)
+        fn.reset = self._reset
+        return fn
+    def _reset(self, item=None):
+        # TODO remove individual item from stash
+        if item is None:
+            print "[memoize] reset cache"
+            self.cache = {}
+        else:
+            print "[memoize] remove %s from cache" % str(item) 
+            self.cache.pop(item, None)      
 
 
 class Message(models.Model):
@@ -247,6 +253,7 @@ class MessageVersion(models.Model):
             if self.parent:
                 self.parent.get_conversation().altered()
         models.Model.save(self)
+        Message.latest_version.reset()
 
 
 class Heap(models.Model):
